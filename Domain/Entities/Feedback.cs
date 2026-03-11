df0f52b720ee0d14db48cfdbc618a02e832a5387
ANXISMORE\anxis <anxismore@gmail.com>
2026-03-11 16:11:51 +0300

    Добавил бд для фидбэка

---
+++ Domain/Entities/Feedback.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Feedback
+{
+    public Guid Id { get; set; }
+    public Guid SessionId { get; set; }
+    public int Mark { get; set; }
+}
---
+++ Infrastructure/Database/Configurations/FeedbackConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configurations;
+
+public class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
+{
+    public void Configure(EntityTypeBuilder<Feedback> builder)
+    {
+        builder.ToTable("feedback");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("id");
+        builder.Property(x => x.Id).HasColumnName("session_id");
+        builder.Property(x => x.Mark).HasColumnName("mark");
+    }
+}
